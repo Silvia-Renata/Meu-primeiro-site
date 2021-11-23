24075f375f840b966db8898aacdaded306908f0e
Silvia-Renata <srv.dossantos@gmail.com>
2021-11-23 19:59:35 -0300

    meu site

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
 # route -> deliciasgeladas.com/contatos
 # função -> o que você quer exibir naquela página
-#template
+# template
 
 @app.route("/")
 def homepage():
@@ -18,12 +20,7 @@ def sabores(sabor_balas):
     return render_template("sabores.html", sabor_balas=sabor_balas)
 
 # colocar o site no ar
-if __name__== "__main__":
-     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
 
-     #servidor do heroku
-
-
-
-
-
+    # servidor do heroku
